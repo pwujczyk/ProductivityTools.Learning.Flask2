1161a6cb47d61e6f0368da862e0841b5b9b305e9
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-04-08 10:54:11 +0200

    reciperesource

--- resources/reciperesource.py
+++ resources/reciperesource.py
@@ -6,3 +6,17 @@ class RecipeResource(Resource):
         recipe=next((recipe for recipe in recipe_list if recipe.id==recipe_id and recipe.is_publish=True),None)
         if recipe is None:
             return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+        return recipe.data, HTTPStatus.OK
+
+    def put(self, recipe_id):
+        data=request.get_json()
+
+        recipe=next((recipe for recipe in recipe_list if recipe.id==recipe_id),None)
+        if recipe is None:
+            return {'message':'recipe not found'}, HTTPStatus.NOT_FOUND
+
+        recipe.name=data['name']
+        recipe.description=data['description']
+        recipe.num_of_servings=data['num_of_servings']
+
+        return recipe.data, HTTPStatus.OK
