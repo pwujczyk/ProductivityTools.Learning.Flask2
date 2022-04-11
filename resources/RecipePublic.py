7863b4e8f48effa241844d19344e7fbaa70bfa5e
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-04-11 07:48:49 +0200

    recipe publish delete

--- resources/RecipePublic.py
+++ resources/RecipePublic.py
@@ -1,3 +1,18 @@
+from http import HTTPStatus
+
 class RecipePublishResource(Resource):
-    def put(self):
-        
+    def put(self, recipe_id):
+        recipe=next((recipe for recipe in recpiple_list if recipe.id==recipe_id),None)
+        if recipe is None:
+            return {'message':'recipe not found'}, HTTPStatus.NOT_FOUND
+        recipe.is_publish=True
+
+        return {}, HTTPStatus.NO_CONTENT
+
+    def __delete__(self, recipe_id):
+        recipe=next((recipe for recipe in recipe_list if recipe.id==recipe_id),None)
+        if recipe is None
+            return {'message':'recipe not found'}, HTTPStatus.NOT_FOUND
+        recipe.is_publish=False
+
+        return {}, HTTPStatus.NO_CONTENT
