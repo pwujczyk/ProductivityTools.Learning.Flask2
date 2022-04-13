90c8078d78eb17f577e89f2092eb477ab2a90fbf
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-04-13 08:57:59 +0200

    ident

--- Models/recipe.py
+++ Models/recipe.py
@@ -15,11 +15,11 @@ class Recipe:
         self.num_of_servings=num_of_servings
         self.is_published=False
 
-@property
-def data(self):
-    return{
-        'id':self.id,
-        'name':self.name,
-        'description':self.description,
-        'num_of_servings':self.num_of_servings
-    }
+    @property
+    def data(self):
+        return{
+            'id':self.id,
+            'name':self.name,
+            'description':self.description,
+            'num_of_servings':self.num_of_servings
+        }
