48e538794b96608e667c6c1330b993f2817e2361
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-04-14 19:23:25 +0200

    app.py

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_restful import Api
+
+from Config import Config
+from extensions import db
+from Models.user import  User
+
 from resources.recipe import RecipeListResource
 from resources.reciperesource import RecipeResource
 from resources.recipepublic import RecipePublishResource
 
-app=Flask(__name__)
+def create_app():
+    app=Flask(__name__)
+    app.config.from_object(Config)
 
-api=Api(app)
+    register_extensions(app)
+    register_resources(app)
+    return app
 
-api.add_resource(RecipeListResource, '/recipes')
-api.add_resource(RecipeResource,'/recipes/<int:recipe_id>')
-api.add_resource(RecipePublishResource,'/recipes/<int:recipe_id>/publish')
+def register_extensions(app):
+    db.init_app(app)
+    migrate=Migrate(app,db)
+
+def register_resources(app):
+    api=Api(app)
+
+    api.add_resource(RecipeListResource, '/recipes')
+    api.add_resource(RecipeResource,'/recipes/<int:recipe_id>')
+    api.add_resource(RecipePublishResource,'/recipes/<int:recipe_id>/publish')
 
 if __name__=='__main__':
+    app=create_app()
     app.run(port=5000,debug=True)
